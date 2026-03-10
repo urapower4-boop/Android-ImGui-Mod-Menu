da78596879f71612beb75c077a26fe830abb4a52
urapower4-boop <urapower4@gmail.com>
2026-03-10 16:18:28 +0200

    Update Main.cpp

--- app/src/main/jni/Main/Main.cpp
+++ app/src/main/jni/Main/Main.cpp
@@ -1,36 +1,42 @@
-//
-// Created by reveny on 21/08/2023.
-//
-
 #include "../Include/KittyMemory/MemoryPatch.h"
 #include "../Include/ImGui.h"
 #include "../Include/RemapTools.h"
-
 #include "../Include/Drawing.h"
 #include "../Include/Unity.h"
 
+// Змінна для кнопки
+bool wallhack = false;
+
 void DrawMenu() {
-    ImGui::ShowDemoWindow();
+    // Назва твого меню
+    ImGui::Begin("HACK FOR SO2");
+
+    // Створюємо кнопку-перемикач
+    if (ImGui::Checkbox("Wallhack (Chams)", &wallhack)) {
+        if (wallhack) {
+            // Твій оффсет для Standoff 2
+            MemoryPatch::createWithHex("libil2cpp.so", 0x1079728, "C0 03 5F D6").Modify();
+        } else {
+            // Відкат до стандартних значень (оригінальний байт)
+            MemoryPatch::createWithHex("libil2cpp.so", 0x1079728, "FD 7B BF A9").Modify();
+        }
+    }
+
+    ImGui::End();
 }
 
 void *thread(void *) {
     LOGI(OBFUSCATE("Main Thread Loaded: %d"), gettid());
+    
+    // Чекаємо 10 секунд, щоб гра встигла завантажити бібліотеки
+    sleep(10); 
+    
     initModMenu((void *)DrawMenu);
-
-    //Hooks, Patches and Pointers here
-    //Example:
-    /*
-     * DobbyHook(getAbsoluteAddress("libIl2cpp.so", 0x0), FunctionExample, old_FunctionExample);
-     * SetAimRotation = (void (*)(void *, Quaternion)) getAbsoluteAddress("libIl2cpp.so", 0x0);
-     */
-
     LOGI("Main thread done");
     pthread_exit(0);
 }
 
-// Call anything from JNI_OnLoad here
 extern "C" {
-    // JNI Support
     JavaVM *jvm = nullptr;
     JNIEnv *env = nullptr;
 
@@ -39,7 +45,6 @@ extern "C" {
         jvm = vm;
         vm->AttachCurrentThread(&env, nullptr);
         LOGI("loadJNI(): Initialized");
-
         return JNI_VERSION_1_6;
     }
 }
@@ -47,10 +52,7 @@ extern "C" {
 __attribute__((constructor))
 void init() {
     LOGI("Loaded Mod Menu");
-
     pthread_t t;
     pthread_create(&t, nullptr, thread, nullptr);
-
-    //Don't leave any traces, remap the loader lib as well
     RemapTools::RemapLibrary("libLoader.so");
 }
